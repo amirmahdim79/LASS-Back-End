3c869ee479ef9351de3bafb70d79c93fd9975845
amir mahdi <mohamadian.am79@ut.ac.ir>
2024-01-31 17:51:09 +0330

    add full path creation

--- routes/paths/index.js
+++ routes/paths/index.js
@@ -1,15 +1,19 @@
 const auth = require('../../middleware/auth')
 const authCheck = require('../../middleware/authCheck');
-const { isSuperAdmin, isSupervisor } = require('../../middleware/permissionCheck');
+const { isSuperAdmin, isSupervisor, hasPermissions } = require('../../middleware/permissionCheck');
 const express = require('express');
 const {
     postCreatePath,
+    postCreateFullPath,
 }
 = require('./services');
 
 const router = express.Router()
 
-// Create new Lab
-router.post('/', isSupervisor, postCreatePath)
+// Create new Path
+router.post('/', hasPermissions(['lab']), postCreatePath)
+
+// Create new FULL Path
+router.post('/', hasPermissions(['lab']), postCreateFullPath)
 
 module.exports = router
--- routes/paths/services.js
+++ routes/paths/services.js
@@ -7,6 +7,9 @@ const { Lab } = require('../../models/lab');
 const { Supervisor } = require('../../models/supervisor');
 const { Path } = require('../../models/path');
 const { Milestone } = require('../../models/milestone');
+const { MILESTONES_FIELD } = require('../milestones/constants');
+const { Task } = require('../../models/task');
+const { TASK_FIELDS } = require('../tasks/constants');
 
 //post create path for lab(Sups)
 const postCreatePath = async (req, res) => {
@@ -29,6 +32,58 @@ const postCreatePath = async (req, res) => {
     res.send(_.pick(path, PATH_FIELDS.CREATE_RES))
 }
 
+//post create full path
+const postCreateFullPath = async (req, res) => {
+    const lab = await Lab.findOne({
+        Supervisor: req.user._id
+    })
+    if (!lab) return res.status(400).send(MESSAGES.LAB_NOT_FOUND)
+
+    let Milestones = req.body.Milestones
+
+    const path = new Path(_.pick(req.body.Path, PATH_FIELDS.CREATE))
+    path.Lab = lab
+
+    path.url = crypto.randomBytes(3).toString('hex')
+
+    Milestones = Milestones.map((milestone) => {
+        const newMilestone = new Milestone({
+            ..._.pick(milestone, MILESTONES_FIELD.CREATE),
+            Path: path._id,
+        })
+
+        let Tasks = milestone.Tasks.map((task) => {
+            const newTask = new Task({
+                ..._.pick(task, TASK_FIELDS.CREATE),
+                Milestone: milestone._id,
+            })
+    
+            return newTask
+        })
+        Task.insertMany(Tasks)
+
+        Tasks.forEach((t) => {
+            newMilestone.Tasks.push(t._id)
+        })
+
+        return newMilestone
+    })
+    await Milestone.insertMany(Milestones)
+
+    Milestones.forEach((m) => {
+        path.Milestones.push(m._id)
+    })
+
+    await path.save()
+
+    lab.Paths.push(path._id)
+
+    lab.save()
+
+    res.send(_.pick(path, PATH_FIELDS.CREATE_RES))
+}
+
 module.exports = {
     postCreatePath,
+    postCreateFullPath,
 }
